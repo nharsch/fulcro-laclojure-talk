f1abb73c4a1771a03ea60d1ede470f5c94839c9b
Nigel Harsch <nigelharsch@gmail.com>
2022-11-09 21:36:14 -0800

    end of talk 1

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -23,14 +23,43 @@
             [cljs.pprint :refer [pprint]]
             )
   )
-
 (defonce app (app/fulcro-app))
 
-(defsc Root [this props]
-  (dom/div "TODO"))
+(defsc Person [this {:person/keys [name age]} {:keys [onDelete]}]
+  {:query         [:person/name :person/age]
+   :initial-state (fn [{:keys [name age] :as params}] {:person/name name :person/age age})}
+  (dom/li
+    (dom/h5 (str name " (age: " age ")") (dom/button {:onClick #(onDelete name)} "X"))))
 
+(def ui-person (comp/factory Person {:keyfn :person/name}))
 
+(defsc PersonList [this {:list/keys [label people]}] ;
+  {:query [:list/label {:list/people (comp/get-query Person)}]
+   :initial-state
+          (fn [{:keys [label]}]
+            {:list/label  label
+             :list/people (if (= label "Friends")
+                                   [(comp/get-initial-state Person {:name "Sally" :age 32})
+                                    (comp/get-initial-state Person {:name "Joe" :age 22})]
+                                   [(comp/get-initial-state Person {:name "Fred" :age 11})
+                                    (comp/get-initial-state Person {:name "Bobby" :age 55})])})}
+  (let [delete-person (fn [name] (println label "asked to delete" name))]
+    (dom/div
+      (dom/h4 label)
+      (dom/ul
+        (map (fn [p] (ui-person (comp/computed p {:onDelete delete-person}))) people)))))
 
+(def ui-person-list (comp/factory PersonList))
+
+(defsc Root [this {:keys [friends enemies]}]
+  {:query         [{:friends (comp/get-query PersonList)}
+                   {:enemies (comp/get-query PersonList)}]
+   :initial-state (fn [params] {:friends (comp/get-initial-state PersonList {:label "Friends"})
+                                :enemies (comp/get-initial-state PersonList {:label "Enemies"})}) }
+  (dom/div
+    (ui-person-list friends)
+    (ui-person-list enemies)))
+(comp/get-query Root)
 
 ;; (defsc Person
 ;;   [this {:person/keys [name age] :as props}]
@@ -55,13 +84,14 @@
 
 
 (comment
-  (pprint (app/current-state app))
   (::app/state-atom app)
+  (pprint (app/current-state app))
   (reset! (::app/state-atom app))
   (app/mount! app Root "fulcro-app")
   (app/schedule-render! app)
   ;; (merge/merge-component! app Person {:person/id :you :person/name "You"})
   (pprint (app/current-state app))
+  (fdn/db->tree [{:friends [:list/label]}] (comp/get-initial-state Root {}) {})
   )
 
 (defn ^:export init []
